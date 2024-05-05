3cc42a43f0d10348c139be04273f154e9a00e0f8
Rafael Pereira Gonçalves <rafael.math14@gmail.com>
2024-05-05 07:47:48 -0300

    Implementações dos exercícios

--- tipos-variaveis/src/TIposVariaveis.java
+++ tipos-variaveis/src/TIposVariaveis.java
@@ -1,5 +1,25 @@
 public class TIposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+
+
+        // Casting (conversão de tipo de variável)
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+
+        short numeroCurto2 = (short) numeroNormal;
+
+        System.out.println(numeroCurto2);
+
+        //Mundando de valor
+
+        int numeroQualquer = 5;
+        numeroQualquer = 10;
+
+        System.out.println(numeroQualquer);
+
+        //Constantes são sempre declaradas em caixa alta e utilizam a palavara chave final na declaração.
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
     }
 }
